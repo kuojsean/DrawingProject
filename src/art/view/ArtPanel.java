f1349990c5db4d8564a90727501121bcf6e4626e
kuojsean <kuo.j.sean@gmail.com>
2018-03-28 08:43:53 -0600

    Started the drawing project.

---
+++ src/art/view/ArtPanel.java
@@ -0,0 +1,22 @@
+package art.view;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+import art.controller.ArtController;
+
+public class ArtPanel extends JPanel
+{
+	private final int MINIMUM_EDGE = 5;
+	private final int MAXIMUM_EDGE = 20;
+	private final int MINIMUM_SCALE = 20;
+	private final int MAXIMUM_SCALE = 100;
+	
+	private ArtController app;
+	
+	
+}
